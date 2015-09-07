1b7542155436144951bd42ec0b27ef002b8724de
Martin Chuckeles <martin@jakubik.info>
2015-09-07 14:37:02 +0200

    Using fingerprint for styles

--- build.js
+++ build.js
@@ -7,11 +7,13 @@ var autoprefixer = require("metalsmith-autoprefixer");
 var branch       = require("metalsmith-branch");
 var cleanCss     = require("metalsmith-clean-css");
 var concat       = require("metalsmith-concat");
+var fingerprint  = require("metalsmith-fingerprint");
 var ignore       = require("metalsmith-ignore");
 var layouts      = require("metalsmith-layouts");
 var markdown     = require("metalsmith-markdown");
 var minify       = require("metalsmith-html-minifier");
 var permalinks   = require("metalsmith-permalinks");
+
 
 
 // setup
@@ -32,12 +34,17 @@ metalsmith(__dirname)
     source: "css",
     destination: "css"
   }))
-  .use(autoprefixer())
-  .use(concat({
-    files: "**/*.css",
-    output: "css/style.min.css"
-  }))
-  .use(cleanCss())
+  .use(branch("**/*.css")
+    .use(autoprefixer())
+    .use(concat({
+      files: "**/*.css",
+      output: "css/style.min.css"
+    }))
+    .use(cleanCss())
+    .use(fingerprint({
+      pattern: "css/style.min.css"
+    }))
+  )
 
   // markdown
   .use(markdown({
@@ -46,14 +53,16 @@ metalsmith(__dirname)
   }))
 
   // html
-  .use(layouts({
-    engine: "handlebars",
-    partials: "includes",
-    layouts: "layouts",
-    pattern: "**/*.html",
-    default: "post.html"
-  }))
-  .use(minify())
+  .use(branch("**/*.html")
+    .use(layouts({
+      engine: "handlebars",
+      partials: "includes",
+      layouts: "layouts",
+      pattern: "**/*.html",
+      default: "post.html"
+    }))
+    .use(minify())
+  )
 
   // permalinks
   .use(permalinks({
